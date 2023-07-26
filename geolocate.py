0943fc9d9459f412588e5cdde46db0566312823d
John Modica <39076455+CybernetiX-S3C@users.noreply.github.com>
2023-07-26 17:38:10 -0400

    Update geolocate.py

--- geolocate.py
+++ geolocate.py
@@ -10,9 +10,22 @@ def geolocate(phone_number):
     else:
         return None
 
+def help():
+    print("This script can be used to geolocate a phone number.")
+    print("To use the script, simply enter the phone number that you want to geolocate.")
+    print("The script will then output the latitude and longitude of the mobile phone.")
+    print("For example, to geolocate the phone number 123-456-7890, you would enter:")
+    print("python geolocate.py 123-456-7890")
+    print("")
+    print("To get help, you can run the script with the -h or --help option.")
+
 if __name__ == "__main__":
-    phone_number = input("Enter the phone number: ")
-    e164_format = phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.E164)
-    latitude, longitude = geolocate(e164_format)
-    print("The latitude is {}.".format(latitude))
-    print("The longitude is {}.".format(longitude))
+    import sys
+    if len(sys.argv) == 2 and sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        help()
+    else:
+        phone_number = input("Enter the phone number: ")
+        e164_format = phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.E164)
+        latitude, longitude = geolocate(e164_format)
+        print("The latitude is {}.".format(latitude))
+        print("The longitude is {}.".format(longitude))
